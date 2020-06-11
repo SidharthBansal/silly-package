8bfe6e49714645f75c24327072478a0b5a60bac1
SidharthBansal <bansal.sidharth2996@gmail.com>
2020-06-11 19:15:55 +0530

    Moved setup out of main

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 import setuptools
 
+
+
+import os
+from pathlib import Path
+import shutil
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+
 setuptools.setup(
     name="mainpackage-sidharthbansal",
-    version="0.0.10",
+    version="0.0.11",
     author="Luke Petschauer",
     author_email="lukewrites@github.com",
     description="A primary example package that is silly.",
     long_description="A primary example package that is silly.",
     url="https://github.com/lukewrites/silly-package/primary",
-    packages=setuptools.find_packages(),
+    packages=find_packages("main"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
